2298d9edfc4dcdcdfea2654f304a577a433069c0
David Padilla <david@crowdint.com>
2013-08-11 12:46:55 -0700

    Clean up the leap code a little bit

--- leap.rb
+++ leap.rb
@@ -5,29 +5,49 @@ FILE_NAME = 'data.txt'
 File.unlink FILE_NAME if File.exists? FILE_NAME
 
 connection :leapmotion, :adaptor => :leapmotion, :port => '127.0.0.1:6437'
-device :leapmotion, :driver => :leapmotion
+device     :leapmotion, :driver  => :leapmotion
 
 work do
   on leapmotion, :frame => :on_frame
 end
 
 def on_frame(*args)
-  frame = args[1]
+  frame      = args[1]
   pointables = frame.pointables
-  gestures = frame.gestures
+  gestures   = frame.gestures
 
-  unless gestures.empty?
-    gesture = gestures.first
-    if gesture.is_a? Artoo::Drivers::Leapmotion::Gesture::Swipe
-      gesture = gestures.first
-      if gesture.state == "stop"
-        #debug(gesture)
-      end
-    end
-  end
+  track_pointables(pointables)
+  #track_gestures(gestures)
+end
 
+def track_pointables(pointables)
   pointables.each do |pointable|
     write_pointable_csv(pointable)
+  end
+end
+
+def track_gestures(gestures)
+  gestures.each do |gesture|
+    write_gesture_csv gesture
+  end
+end
+
+def write_pointable_csv(pointable)
+  CSV.open FILE_NAME, 'a+' do |csv|
+    csv << pointable.stabilizedTipPosition
+  end
+end
+
+def write_gesture_csv(gesture)
+  if gesture.is_a? Artoo::Drivers::Leapmotion::Gesture::Swipe
+    if gesture.state == "stop"
+      #debug(gesture)
+      puts "Swipe"
+      CSV.open FILE_NAME, 'a+' do |csv|
+        csv << gesture.startPosition
+        csv << gesture.position
+      end
+    end
   end
 end
 
@@ -37,10 +57,4 @@ def debug(gesture)
   puts gesture.direction.inspect
   puts gesture.type.inspect
   puts gesture.state.inspect
-end
-
-def write_pointable_csv(pointable)
-  CSV.open FILE_NAME, 'a+' do |csv|
-    csv << pointable.stabilizedTipPosition
-  end
 end
